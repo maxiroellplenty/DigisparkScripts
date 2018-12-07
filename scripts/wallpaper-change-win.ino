2a53630611b52b3de047ec61e03d9e80252a88c5
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2018-12-07 17:18:24 +0100

    Update wallpaper-change-win.ino

--- scripts/wallpaper-change-win.ino
+++ scripts/wallpaper-change-win.ino
@@ -10,6 +10,7 @@ void setup()
   DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
   pn("powershell", 500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(500);
